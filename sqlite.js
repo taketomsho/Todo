d88f9f9f4c2542f7cc5791cd474adfba4216a963
taketomsho <shotarotaketom@gmail.com>
2020-04-09 08:36:27 +0900

    gitignore is working

--- sqlite.js
+++ sqlite.js
@@ -1,22 +1,21 @@
 const sqlite3 = require('sqlite3').verbose();
  
 // open database in memory
-let db = new sqlite3.Database('./db/filelist.db', (err) => {
+let db = new sqlite3.Database('./db/task.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Connected to the filelist database.');
 });
 
-db.serialize(() => {
-    db.each(`SELECT directory,foldersize
-             FROM foldersizes`, (err, row) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log(row.directory + "\t" + row.foldersize);
-    });
-});
+// db.serialize(() => {
+//     db.each(`SELECT * FROM tasks`, (err, row) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//       console.log(row.task_id + "\t" + row.title + "\t" + row.content);
+//     });
+// });
 
 // close the database connection
 db.close((err) => {
